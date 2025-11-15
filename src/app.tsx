f3d97b7bed4c73fa966e71800ebce7f7e974eae6
thalhahs <thalhah@gmail.com>
2025-11-15 12:52:25 +0700

    feat: profile props

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,14 @@ export function App() {
   return (
     <div>
       <h1>Task Management</h1>
-      <Profile />
+      <Profile
+        imageUrl="https://unsplash.com/photos/bald-man-in-sunglasses-dancing-in-studio-SQ5SFrjoXy4"
+        name="Karsten Winegeart"
+      />
+      <Profile
+        imageUrl="https://unsplash.com/photos/woman-reading-a-book-in-a-modern-armchair-4dLKoASYgIk"
+        name="Caroline Badran"
+      />
     </div>
   );
 }
--- src/profile.tsx
+++ src/profile.tsx
@@ -1,6 +1,9 @@
-export function Profile() {
-  const imageUrl = "https://unsplash.com/photos/bald-man-in-sunglasses-dancing-in-studio-SQ5SFrjoXy4";
-  const name = "Karsten Winegeart";
-
-  return <img src={imageUrl} alt={name} />;
+export function Profile({
+  imageUrl,
+  name,
+}: {
+  imageUrl: string;
+  name: string;
+}) {
+  return <img src={imageUrl} alt={name} width={300} />;
 }
