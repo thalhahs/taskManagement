287d7a89715cbbbe9ab9a40c3aa441fde5e7a03b
thalhahs <thalhah@gmail.com>
2025-11-15 12:42:36 +0700

    feat: profile component

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,10 @@
+import { Profile } from "./profile";
+
 export function App() {
   return (
     <div>
       <h1>Task Management</h1>
+      <Profile />
     </div>
   );
 }
---
+++ src/profile.tsx
@@ -0,0 +1,8 @@
+export function Profile() {
+  return (
+    <img
+      src="https://unsplash.com/photos/a-large-clock-tower-towering-over-the-city-of-london-ZPm77Deio00"
+      alt="London Clock Tower"
+    />
+  );
+}
